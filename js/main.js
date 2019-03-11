8240a0a6aca29a8bf7d093741e30e73515a2d570
MotuMoorea <martincaroline69@gmail.com>
2019-03-11 20:43:30 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    $('.scroll').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+    
+    
+});
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("upButton").style.display = "block";
+    } else {
+        document.getElementById("upButton").style.display = "none";
+    }
+}
